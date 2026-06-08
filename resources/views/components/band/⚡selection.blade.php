1fcc23dc6e6653d068cc12ae2269a92749cca49e
DWave3239 <marvin.hengster@gmx.at>
2026-06-08 20:34:40 +0200

    temporary app icon // header band switch

---
+++ resources/views/components/band/⚡selection.blade.php
@@ -0,0 +1,47 @@
+<?php
+
+use App\Models\User;
+use App\Models\BandUser;
+use Livewire\Attributes\Computed;
+use Livewire\Attributes\Session;
+use Livewire\Component;
+use Illuminate\Support\Facades\Auth;
+
+new class extends Component
+{
+    #[Session]
+    public ?int $currentBand = null;
+
+    #[Computed]
+    public function bands()
+    {
+        $bands = User::find(Auth::id())
+            ->bands()
+            ->get();
+        
+        if (count($bands) == 1) {
+            $this->currentBand = $bands[0]->id;
+        }
+        
+        return $bands;
+    }
+
+    public function setCurrentBand()
+    {
+        // reroute to the dashboard after changing the current band
+        return $this->redirect('/dashboard', navigate: true);
+    }
+};
+?>
+
+<div>
+    @if (count($this->bands) > 1)
+        <select wire:model="currentBand" wire:change="setCurrentBand">
+        @foreach ($this->bands as $band)
+            <option wire:key="{{ $band->id }}" value="{{ $band->id }}">{{ $band->name }}</option>
+        @endforeach
+        </select>
+    @else
+        <div>{{ $this->bands[0]->name }}</div>
+    @endif
+</div>
